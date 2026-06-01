1096538ee171192181380aa45fe8429a486b5ff1
iCleyvin <cleyvinos@gmail.com>
2026-06-01 10:46:50 -0400

    feat: CompanyName=cleyvinOS en recursos del exe

--- crates/server/build.rs
+++ crates/server/build.rs
@@ -3,6 +3,10 @@ fn main() {
     {
         let mut res = winresource::WindowsResource::new();
         res.set_icon("../../assets/icon.ico");
-        res.compile().expect("failed to embed icon");
+        res.set("CompanyName",      "cleyvinOS");
+        res.set("FileDescription",  "Yzendris KVM Server");
+        res.set("ProductName",      "Yzendris");
+        res.set("LegalCopyright",   "cleyvinOS");
+        res.compile().expect("failed to embed resources");
     }
 }
